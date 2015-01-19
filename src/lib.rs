6a303f69e45e102310ddccd7b53ae41b92693e44
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2015-01-19 13:19:11 +0200

    where_ signature: use plain `self`, not `&self`

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub struct List<T> {
 }
 
 impl<T> List<T> where T: Clone {
-    pub fn where_<F>(&self, f: F) -> List<T> where F: Fn(&T) -> bool {
+    pub fn where_<F>(self, f: F) -> List<T> where F: Fn(&T) -> bool {
         let mut tmp = Vec::new();
         for item in self.items.iter() {
             if f(item) {
@@ -16,7 +16,7 @@ impl<T> List<T> where T: Clone {
 
 #[test]
 fn where_bool_true() {
-    let xs = List { items: vec![1u8,2,3] };
+    let xs = List { items: vec![1,2,3] };
     let filtered = xs.where_(|x| *x % 2 == 0);
     assert_eq!(filtered.items.len(), 1);
 }
