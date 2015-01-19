57603c4e6e0e9ed07f2c8d2be51baa7cdba4c413
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2015-01-19 23:39:43 +0200

    more readable closures (borrowed parameters)

--- src/lib.rs
+++ src/lib.rs
@@ -32,26 +32,26 @@ mod tests {
     #[test]
     fn where_even_number() {
         let xs = List { items: vec![1,2,3] };
-        let filtered = xs.where_(|x| *x % 2 == 0);
+        let filtered = xs.where_(|&x| x % 2 == 0);
         assert_eq!(filtered.items.len(), 1);
     }
 
     #[test]
     fn select_half_negative() {
         let xs = List { items: vec![1u8,2,3] };
-        let ys = xs.select(|x| (*x as f32) / -2.0);
+        let ys = xs.select(|&x| (x as f32) / -2.0);
         assert_eq!(ys.items[0], -0.5);
     }
 
     #[bench]
     fn bench_select_id(b: &mut Bencher) {
         let xs = List { items: vec![1u32,2,3] };
-        b.iter(|| xs.select(|x| *x));
+        b.iter(|| xs.select(|&x| x));
     }
 
     #[bench]
     fn bench_where_even(b: &mut Bencher) {
         let xs = List { items: vec![1u32,2,3] };
-        b.iter(|| xs.where_(|x| *x % 2 == 0));
+        b.iter(|| xs.where_(|&x| x % 2 == 0));
     }
 }
